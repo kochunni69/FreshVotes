dc1dfad876c8007691f89dc6273a21e7afbfb543
kochunni69 <velappan_pillai@yahoo.com>
2021-02-21 23:18:17 -0600

    Commiting Dashboard Controller and properties file

---
+++ FreshVotes/src/main/java/com/FreshVotes/DashboardController.java
@@ -0,0 +1,15 @@
+package com.FreshVotes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashboardController {
+	
+	@RequestMapping( value="/", method=RequestMethod.GET)
+	private String rootView() {
+		
+		return "index";
+	}
+}
